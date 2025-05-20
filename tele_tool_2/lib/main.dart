267b504eaf5e31a94ceba09eaf7812a31ef1eea3
yizhouzhao <yizhou_zhao@berkeley.edu>
2025-05-20 09:42:17 -0700

    v1

--- tele_tool_2/lib/main.dart
+++ tele_tool_2/lib/main.dart
@@ -1,128 +1,167 @@
+import 'dart:io';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:arkit_plugin/arkit_plugin.dart';
+import 'package:vector_math/vector_math_64.dart' as vector_math;
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  final bool isDebug = true; // Add this variable to control debugPrint
+  final TextEditingController _ipAddressController = TextEditingController();
+  final TextEditingController _portController = TextEditingController();
+  Socket? _socket;
+  String _message = 'Not Connected.';
+  bool _isConnected = false;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  late final ARKitController arkitController;
+  late final Ticker _ticker;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  Duration _previousTimestamp = Duration.zero;
+  final Duration _frameInterval = const Duration(milliseconds: 33); // ~30 fps
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void initState() {
+    super.initState();
+    _ipAddressController.text = '192.168.1.100'; // Or any default IP
+    _portController.text = '12345'; // Or any default port
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    _ticker = createTicker((elapsed) {
+      final delta = elapsed - _previousTimestamp;
+      if (delta >= _frameInterval) {
+        _previousTimestamp = elapsed;
+        _update(); // Your 30 FPS function here
+      }
     });
+
+    _ticker.start();
+  }
+
+  Future<void> _update() async {
+    // This function is called ~30 times per second
+    if (isDebug) debugPrint('Tick at ${DateTime.now()}');
+    if (arkitController != null) {
+      // Perform ARKit updates here
+      final camera_pos = await arkitController.cameraPosition();
+      if (camera_pos == null) {
+        if (isDebug) debugPrint('Camera position is null');
+        return;
+      }
+      if (isDebug) debugPrint('Camera position: $camera_pos');
+
+      final camera_rot = await arkitController.getCameraEulerAngles();
+      if (camera_rot == null) {
+        if (isDebug) debugPrint('Camera rotation is null');
+        return;
+      }
+      if (isDebug) debugPrint('Camera rotation: $camera_rot');
+    }
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+  void dispose() {
+    arkitController.dispose();
+    _disconnect(); // Ensure disconnection on dispose
+    _ipAddressController.dispose();
+    _portController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _connect() async {
+    try {
+      setState(() {
+        _message = 'Connecting...';
+      });
+      final ipAddress = _ipAddressController.text;
+      final port = int.parse(_portController.text);
+      _socket = await Socket.connect(
+        ipAddress,
+        port,
+        timeout: const Duration(seconds: 5),
+      ); //add timeout
+      setState(() {
+        _isConnected = true;
+        _message = 'Connected to: ${ipAddress}:${port}';
+      });
+      // Start sending immediately after connect
+      if (_isConnected && _socket != null) {
+        _socket?.add(
+          utf8.encode('hello\n'),
+        ); // Add newline for server to recognize end of message.
+        _socket?.flush(); // Ensure data is sent
+      }
+    } catch (e) {
+      setState(() {
+        _isConnected = false;
+        _message = 'Error connecting: $e';
+      });
+      _disconnect(); //clean up
+    }
+  }
+
+  void _disconnect() {
+    try {
+      _socket?.close();
+    } catch (e) {
+      if (isDebug) print('Error disconnecting: $e');
+    } finally {
+      _socket = null;
+      setState(() {
+        _isConnected = false;
+        _message = 'Disconnected.';
+      });
+      // _timer?.cancel(); // Remove timer cancel
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(title: const Text('ARKit in Flutter')),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            controller: _ipAddressController,
+            decoration: const InputDecoration(labelText: 'IP Address'),
+          ),
+          TextField(
+            controller: _portController,
+            decoration: const InputDecoration(labelText: 'Port'),
+            keyboardType: TextInputType.number,
+          ),
+          ElevatedButton(
+            onPressed: _isConnected ? _disconnect : _connect,
+            child: Text(_isConnected ? 'Disconnect' : 'Connect'),
+          ),
+          Text(_message),
+          SizedBox(
+            width: 300, // Set the desired width
+            height: 400, // Set the desired height
+            child: ARKitSceneView(onARKitViewCreated: onARKitViewCreated),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            // A button with an uparrow icon
-            IconButton(
-              icon: const Icon(Icons.arrow_upward),
-              tooltip: 'Increment',
-              onPressed: _incrementCounter,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ),
+  );
+
+  void onARKitViewCreated(ARKitController arkitController) {
+    this.arkitController = arkitController;
+    final node = ARKitNode(
+      geometry: ARKitSphere(radius: 0.1),
+      position: vector_math.Vector3(0, 0, -0.5),
     );
+    this.arkitController.add(node);
   }
 }
